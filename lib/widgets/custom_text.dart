035ae1c05bacc0fdbd626a444f7d3f68f6262403
ivgl <iivgll4@gmail.com>
2023-01-28 02:17:14 +0700

    initial commit

---
+++ lib/widgets/custom_text.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class CustomText extends StatelessWidget {
+  const CustomText(
+      {super.key,
+      required this.text,
+      this.size,
+      this.fontWeight,
+      this.align,
+      this.color,
+      this.underline});
+
+  final FontWeight? fontWeight;
+  final double? size;
+  final String text;
+  final TextAlign? align;
+  final Color? color;
+  final TextDecoration? underline;
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(text,
+        textAlign: align,
+        style: GoogleFonts.montserrat(
+          fontSize: size,
+          fontWeight: fontWeight,
+          color: color,
+          decoration: underline,
+        ));
+  }
+}
