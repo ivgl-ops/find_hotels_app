ff1180f8703c0dbac2f45f88b59510c952b337dc
ivgl <iivgll4@gmail.com>
2023-02-01 00:21:40 +0700

    Connect Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,14 @@
-import 'package:find_hotels_app/view/main_view.dart';
-import 'package:flutter/material.dart';
 
-void main() {
+import 'package:find_hotels_app/view/test.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
@@ -14,7 +21,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MainView(),
+      home:  AddData(),
     );
   }
 }
