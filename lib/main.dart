28bae81a76d97fbba71c8c13b27ec48331291fab
ivgl <iivgll4@gmail.com>
2023-02-01 01:30:29 +0700

    Configured Firebase in project

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
-
+import 'package:find_hotels_app/view/main_view.dart';
 import 'package:find_hotels_app/view/test.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:provider/provider.dart';
 import 'firebase_options.dart';
 
 void main() async {
@@ -16,12 +17,12 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home:  AddData(),
+    return  MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: const MainView(),
     );
   }
 }
--- lib/view/main_view.dart
+++ lib/view/main_view.dart
@@ -2,10 +2,10 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:find_hotels_app/widgets/custom_text.dart';
 import 'package:flutter/material.dart';
 
-
 class MainView extends StatefulWidget {
-  final String name;
-   const MainView({super.key, required this.name});
+  const MainView({
+    super.key,
+  });
 
   @override
   State<MainView> createState() => _MainViewState();
@@ -15,38 +15,28 @@ class _MainViewState extends State<MainView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Container(
-            height: 250,
-            decoration: const BoxDecoration(
-                gradient: LinearGradient(
-              begin: Alignment.topRight,
-              end: Alignment.bottomLeft,
-              colors: [
-                Colors.blue,
-                Colors.red,
-              ],
-            )),
-            child: Column(
-              children: [
-                Container(
-                  margin: const EdgeInsets.only(top: 30, left: 50, right: 10),
-                  child: const CustomText(
-                    text:
-                        "Найдите лучшие предложения на отели,\nапартаменты и многое другое...",
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white,
-                    size: 20,
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ],
+        body: Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.green,
+        title: const Text("Hotels"),
       ),
-    );
+      body: StreamBuilder(
+        stream: FirebaseFirestore.instance.collection('test').snapshots(),
+        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+
+          if (!snapshot.hasData) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+
+          return ListView(
+            children: snapshot.data!.docs.map((document) {
+              return Center(child: Text(document['name']));
+            }).toList(),
+          );
+        },
+      ),
+    ));
   }
 }
