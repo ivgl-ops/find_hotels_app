216d3e203e5043ec5ff2244ba08bfdc9597eb1f4
ivgl <iivgll4@gmail.com>
2023-02-01 13:46:31 +0700

    building Main View

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
+import 'package:find_hotels_app/firebase/get_hotels.dart';
 import 'package:find_hotels_app/view/main_view.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:provider/provider.dart';
-import 'firebase_options.dart';
+import 'firebase/firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -16,12 +17,15 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return  MaterialApp(
+    return MultiProvider(
+      providers: [ChangeNotifierProvider(create: (context) => GetHotel())],
+      child: MaterialApp(
         title: 'Flutter Demo',
         theme: ThemeData(
           primarySwatch: Colors.blue,
         ),
         home: const MainView(),
+      ),
     );
   }
 }
