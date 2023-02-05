546ffa509b5d058a5bb599fd47eb7a8ce77668c0
ivgl <iivgll4@gmail.com>
2023-02-05 23:28:36 +0700

    create filter

---
+++ lib/view/filter_hotels.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class Filter_Hotels extends StatefulWidget {
+  const Filter_Hotels({super.key});
+
+  @override
+  State<Filter_Hotels> createState() => _Filter_HotelsState();
+}
+
+class _Filter_HotelsState extends State<Filter_Hotels> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
